889a9aeec3446fd6257022d4dfad8afa7ff74042
Luiz Fernando <fernandoluiz43673@gmail.com>
2022-10-20 17:33:49 -0300

    implemented the html loader
    Signed-off-by: Luiz Vicente <fernandoluiz5217@gmail.com>

--- 2-loaders/src/index.js
+++ 2-loaders/src/index.js
@@ -1,6 +1,7 @@
 import Title from './components/title.js';
 import Image from './components/img.js';
 import Button from './components/button.js';
+import Warning from './templates/warning.html';
 
 const title = new Title();
 title.create("Primeira página");
@@ -11,10 +12,14 @@ image.insertImg();
 const btn = new Button();
 btn.create();
 
-//babel spreads
+// babel spreads
 const obj = {a: 1, b:2, c:3, d:4};
 
 let {a, b, ...teste} = obj;
 console.log(a);
 console.log(b);
 console.log(teste);
+
+// html
+const body = document.querySelector('body');
+body.innerHTML += Warning;
