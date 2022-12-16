63210c45c1fa5e55900345b7527e1d2d5ca64034
Luiz Fernando <fernandoluiz43673@gmail.com>
2022-12-16 12:52:26 -0300

    created integration with react
    Signed-off-by: Luiz Vicente <dev.vicente.contato@gmail.com>

---
+++ 7-react-integration/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Meu projeto de React com Webpack</h1>
+        <p>Eu nunca usei React antes mas parece legal</p>
+      </div>
+    );
+  }
+}
+
+export default App
